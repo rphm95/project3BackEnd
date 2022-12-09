35d6959fd794b4aacedbf4e68144dcc02a4e97e6
rafaela bootcamp <rafaelabootcamp@rafaelas-air.myfiosgateway.com>
2022-12-09 15:54:44 -0500

    backend working

---
+++ models/seedClothes.js
@@ -0,0 +1,21 @@
+module.exports = [
+    {
+        name: "DRAPED SHEATH DRESS",
+        price: 70,
+        store: "ZARA",
+        image: "https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013",
+        like: false,
+        link: "https://www.zara.com/us/en/draped-sheath-dress-p08536520.html?v1=200060682",
+        type: "Dress"
+    },
+
+    {
+        name: "ULTRA HIGH-RISE RIPPED LIGHT WASH DAD JEANS",
+        price: 60,
+        store: "Hollister",
+        image: "https://img.hollisterco.com/is/image/anf/KIC_355-2162-6506-281_model2?policy=product-large",
+        like: false,
+        link: "https://www.hollisterco.com/shop/us/p/ultra-high-rise-dad-jeans-50414962?seq=04&categoryId=12623&faceout=model",
+        type: "Dress"
+    }
+]
