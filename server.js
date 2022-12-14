14aeaccf89c4bf4ec9e8f0a3098f213cc0c92ebf
rafaela bootcamp <rafaelabootcamp@rafaelas-air.myfiosgateway.com>
2022-12-13 19:50:34 -0500

    linked netlify

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin:'http://localhost:3001',
+        origin:'https://glittering-paletas-4673fa.netlify.app',
         credentials:true
     }
 ));
